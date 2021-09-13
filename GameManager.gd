0f365814c774c595c59110599e5427de698fd897
ZayLong <zaylong10@gmail.com>
2021-09-13 00:34:00 -0400

    init commit

---
+++ GameManager.gd
@@ -0,0 +1,142 @@
+extends Node2D
+
+
+export(PackedScene) var bingo_card
+
+onready var current_number_display:Label = $HUD/Label
+onready var pick_ball_button:Button = $HUD/Button
+onready var evaluate_button:Button = $HUD/Button2
+
+var bingo_basket:Array = []
+var called_balls:Array = []
+var bingo_cards:Array = []
+var current_number:String
+
+signal picked_ball_event
+
+# to start...first to connnect becomes host, yet cannot be a "player" in the sense that they will not be given a bingo card
+# their job will only be to evaluate cards n stuff. Hopefully this will work headless...
+
+# NETWORK RELATED PROPERTIES
+# Default game server port. Can be any number between 1024 and 49151.
+# Not on the list of registered or common ports as of November 2020:
+# https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+const DEFAULT_PORT = 10567
+
+# Max number of players.
+const MAX_PEERS = 12
+
+var peer = null
+
+var SERVER_IP = "68.55.157.177"
+
+func _ready():
+	# allows us to get "true" random numbers by using a time based seed.
+	# only needs to be ran once in the entire application
+	randomize()
+	#fill_basket(3)
+	
+	peer = NetworkedMultiplayerENet.new()
+	# Running on the headless server platform
+	if OS.has_feature("Server"):
+		print("STARTING SERVER %s ON PORT %s" % [SERVER_IP, DEFAULT_PORT])
+		peer.create_server(DEFAULT_PORT, MAX_PEERS)
+		get_tree().set_network_peer(peer)
+		get_tree().connect("network_peer_connected", self,"_network_peer_connected")
+		get_tree().connect("network_peer_disconnected", self, "_network_peer_disconnected")
+	else:
+		print("CONNECTING TO SERVER %s ON PORT %s" % [SERVER_IP, DEFAULT_PORT])
+		peer.create_client(SERVER_IP, DEFAULT_PORT)
+		get_tree().network_peer = peer
+		get_tree().connect("connection_failed", self, "_connection_failed")
+		get_tree().connect("connected_to_server", self, "_connected_to_server")
+
+
+
+
+
+	
+	# create player
+	if bingo_card && false:
+		var x = bingo_card.instance()
+		x._init(bingo_basket, 3)
+		# connect our picked_ball event to our newly created bingo cards set_current_ball setter method
+		# this way whenever we pick a ball, we will emit a signal telling all our subscribers what we picked
+		self.connect("picked_ball_event", x, "set_current_ball")
+		add_child(x)
+		bingo_cards.append(x)
+	
+	# UI button event setup
+	if pick_ball_button:
+		# connect the buttons pressed event to our pick_ball method
+		pick_ball_button.connect("pressed", self, "pick_ball")
+	if evaluate_button:
+		evaluate_button.connect("pressed", self, "evaluate_bingo_cards")
+
+	pass
+
+func _network_peer_connected(id:int):
+	print("A USER WITH ID: %s CONNECTED" % [id])
+	pass
+
+func _network_peer_disconneted(id:int):
+	print("A USER WITH ID: %s DISCONNECTED" % [id])
+	pass
+
+func _connection_failed():
+	print("CONNECTION TO SERVER FAILED FOR SOME REASON")
+	pass
+
+func _connected_to_server():
+	print("CONNECTION SUCCESS!")
+	pass
+# populate all the numbers/letter
+master func fill_basket(number_of_balls:int)->void:
+	# clear out left over "balls" from our basket first
+	bingo_basket.clear()
+	called_balls.clear()
+
+	# populate the letter array
+	# 0 to x
+	for n in number_of_balls:
+		n = n as String
+		if number_of_balls >= 1: 
+			bingo_basket.append("B" + n)
+		if number_of_balls >= 2: 
+			bingo_basket.append("I" + n)
+		if number_of_balls >= 3: 
+			bingo_basket.append("N" + n)
+		if number_of_balls >= 4: 
+			bingo_basket.append("G" + n)
+		if number_of_balls >= 5: 
+			bingo_basket.append("O" + n)
+		pass
+
+	pass
+
+# pick a letter then pick a number, 
+# remove said leternumber combo from the overall pool
+# return the number we got
+master func pick_ball()->void:
+	
+	if bingo_basket.size() <= 0:
+		print("No balls left")
+		return
+	# start mixing up our balls in the basket, turn the crank!
+	bingo_basket.shuffle()
+	
+	# pick a ball and remove it from the basket!
+	var picked_ball = bingo_basket.pop_front()
+	current_number = picked_ball
+	called_balls.append(picked_ball)
+	
+	current_number_display.text = current_number
+	emit_signal("picked_ball_event", current_number)
+	pass
+
+# player may request for their card to be evaluated
+remote func evaluate_bingo_cards()->void:
+	for card in bingo_cards:
+		
+		pass
+	pass
---
+++ bingo_card.gd
@@ -0,0 +1,184 @@
+extends ItemList
+class_name Bingo_Card
+# PROPERTIES ===================================================================
+var item_list
+var bingo_card:Array
+var current_basket:Array
+var grid_size:int
+var bingo_card_data = {
+	"cells":[]
+}
+var current_ball setget set_current_ball
+
+# CORE =========================================================================
+func _init(bingo_basket:Array = [], grid_width:int = 0):
+	grid_size = grid_width
+	current_basket = bingo_basket
+	max_columns = grid_width
+	pass
+
+func _ready():
+	generate_bingo_card(current_basket)
+	connect("item_selected", self, "item_selected")
+	pass
+
+# METHODS ======================================================================
+func set_current_ball(ball):
+	current_ball = ball
+	pass
+
+func item_selected(index:int):
+	print(array_index_to_grid(index, grid_size))
+	if self.get_item_text(index) == current_ball:
+		set_item_disabled(index, true)
+		set_item_selectable(index, false)
+		bingo_card_data["cells"][index].filled = true
+		evaluate_card(index)
+	pass
+
+# evaluate bingo card based on the position of a given cell
+# basically does the cell we just filled in complete a row or column (also generically checks for diagnal matches)
+func evaluate_card(index:int)->void:
+	# if any of these match grid_size, then we got a bingo!
+	var row_count:int = 0
+	var column_count:int = 0
+	var right_to_left_count:int = 0
+	var left_to_right_count:int = 0
+	
+	# tests if we have bingo via row
+	# gives us the number closest to our current index thats divisible by our grid size
+	# we do this so we can find the first element of the row of a given index
+
+	var target_index = index - (index % grid_size)
+	if target_index % grid_size == 0:
+		# this is most likely  the left most element in a row
+		for n in grid_size:
+			if bingo_card_data["cells"][target_index + n].filled == true:
+				row_count += 1
+			pass
+	
+	if row_count == grid_size:
+		print("%s IN A ROW! THATS BINGO!" % [grid_size])
+		#return
+	# check if we have bingo va column
+	# convert our index to a vector2, then iterate from zero to grid_size(minus 1) 
+	# we can use our converrted current index x pos, then iterate the y to get all the cells in a column
+	# then we just evaluate, easy!
+
+	var current_index_pos:Vector2 = array_index_to_grid(index, grid_size)
+	for m in grid_size:
+		var target_pos:Vector2 = Vector2(current_index_pos.x, m)
+		var target_pos_to_index:int = grid_to_array_index(target_pos, grid_size)
+
+		if bingo_card_data["cells"][target_pos_to_index].filled == true:
+			column_count += 1
+		pass
+	
+	if column_count == grid_size:
+		print("%s IN A COLUMN! THATS BINGO!" % [grid_size])
+		#return
+	# check if we have bingo diagnally 
+	# theres only ever going to be 2 ways to get diagnal 
+	# so we either gotta start from the top left (0,0) or top right (grid_size, 0)
+	# checks diagnal from right to left
+	for o in grid_size:
+		var p:Vector2 = Vector2(o,o)
+		var i:int = grid_to_array_index(p, grid_size)
+		if bingo_card_data["cells"][i].filled == true:
+			right_to_left_count += 1
+		pass
+	
+	if right_to_left_count == grid_size:
+		print("%s THERE! FROM THE RIGHT, DIAGNALLY! BINGO!!" % [right_to_left_count])
+		#return
+	
+	 # checks diagnal from left to right
+	for q in grid_size:
+		var pv:Vector2 = Vector2((grid_size - 1) - q, q)
+		var iv:int = grid_to_array_index(pv, grid_size)
+		if bingo_card_data["cells"][iv].filled == true:
+			left_to_right_count += 1
+		pass
+	pass
+	
+	if left_to_right_count == grid_size:
+		print("%s THERE! FROM THE LEFT, DIAGNALLY! BINGO!!" % [left_to_right_count])
+		#return
+	
+	pass
+
+func generate_bingo_card(bingo_basket:Array)->void:
+	if bingo_basket.size() == 0:
+		return
+
+	var b_array:Array = []
+	var i_array:Array = []
+	var n_array:Array = []
+	var g_array:Array = []
+	var o_array:Array = []
+	
+	# filter and sort out out the balls into different arrays based on letter
+	for ball in bingo_basket:
+		# do a search within basket_instance for an element that starts with B
+		if "B" in ball:
+			b_array.append(ball)
+		if "I" in ball:
+			i_array.append(ball)
+		if "N" in ball:
+			n_array.append(ball)
+		if "G" in ball:
+			g_array.append(ball)
+		if "O" in ball:
+			o_array.append(ball)
+		pass
+	
+	# pick one of these "B" elements at random
+	# remove said element from basket_instance
+	# add element to bingo_card array
+	# iterate 25 times because we have 25 times on the card
+	# 
+	for n in grid_size:
+		if !b_array.empty():
+			bingo_card.append(b_array[randi() % b_array.size()])
+			b_array.remove(b_array.find(bingo_card.back()))
+		
+		if !i_array.empty():
+			bingo_card.append(i_array[randi() % i_array.size()])
+			i_array.remove(i_array.find(bingo_card.back()))
+		
+		if !n_array.empty():
+			bingo_card.append(n_array[randi() % n_array.size()])
+			n_array.remove(n_array.find(bingo_card.back()))
+		
+		if !g_array.empty():
+			bingo_card.append(g_array[randi() % g_array.size()])
+			g_array.remove(g_array.find(bingo_card.back()))
+		
+		if !o_array.empty():
+			bingo_card.append(o_array[randi() % o_array.size()])
+			o_array.remove(o_array.find(bingo_card.back()))
+		pass
+	
+	# ok we have our bingo card array now.
+	# next thing to do is populate our ItemList with our bingo_card array
+	
+	for cell in bingo_card:
+		bingo_card_data["cells"].append({"name": cell, "filled": false})
+		self.add_item(cell)
+		pass
+	
+	# grid_size = sqrt(bingo_card.size())
+	pass
+
+# converts the index of an array to its x,y equivalent
+# grid_width is the width of the grid, i.e. a 5 x 5 grid would have a width of 5
+func array_index_to_grid(index:int, grid_width:int)->Vector2:
+	var x_column = index % grid_width
+	var y_row = floor(index / grid_width)
+	return Vector2(x_column, y_row)
+	pass
+
+
+func grid_to_array_index(pos:Vector2, grid_width:int)->int:
+	return int((pos.y * grid_width) + pos.x)
+	pass
